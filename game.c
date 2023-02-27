63737677d374f71bab8ee3c6729d0b0e84cfeddf
Oscar Gertling <gertling@n133-p127.eduroam.kth.se>
2023-02-27 13:13:06 +0100

    update

--- game.c
+++ game.c
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "header.h"
-
+#include <math.h>
 
 bool isPlaying = false;
 
@@ -16,18 +16,19 @@ double ballSpeedX = 1;
 
 double player1Y;
 double player2Y;
+
 double playerHeight = 5;
 
 void initGame() // Will most likely be used to reset game
 {
   isPlaying = false;
 
-  gameWidth = 40;  // Screen width
-  gameHeight = 20; // Screen height
+  gameWidth = 128; // Screen width
+  gameHeight = 32; // Screen height
 
   // Start in middle of screen
-  ballX = gameWidth / 2;    // Make ball start in middle
-  ballY = gameHeight / 2;   // Make ball start in middle
+  ballX = gameWidth / 2;  // Make ball start in middle
+  ballY = gameHeight / 2; // Make ball start in middle
 }
 
 void containPlayer(double *playerPointer, double upperLimit)
@@ -69,4 +70,32 @@ void tickGame()
 {
 
   updatePlayerPos(3, 1);
+}
+
+int myRound(double number)
+{
+  int rounded = floor(number);
+  if (number - rounded >= 0.5)
+  {
+    return rounded + 1;
+  }
+  else
+  {
+    return rounded;
+  }
+}
+
+int getPlayerPosition(int playerNumber)
+{
+  if (playerNumber == 1)
+  {
+    int player1 = myRound(player1Y); // Player position as int.
+    return player1;
+  }
+  if (playerNumber == 2)
+  {
+
+    int player2 = myRound(player2Y); // Player position as int.
+    return player2;
+  }
 }
